35f8a03dae33329a46a57b539381b3a60b3482b7
Rob Chandler <robkadt@gmail.com>
2017-02-24 17:27:33 +0000

    Switch out csv parser for one that tolerates escaping in openflights data

--- convert.js
+++ convert.js
@@ -1,19 +1,37 @@
-var csv = require('csv');
-var JSONStream = require('JSONStream');
-var fs = require('fs');
-var _ = require('lodash');
+var csv = require("csv-parser");
+var JSONStream = require("JSONStream");
+var fs = require("fs");
+var _ = require("lodash");
 
-var columns = ['id', 'name', 'city', 'country', 'iata', 'icao', 'latitude', 'longitude', 'altitude', 'timezone', 'dst', 'tz'];
+var columns = [
+  "id",
+  "name",
+  "city",
+  "country",
+  "iata",
+  "icao",
+  "latitude",
+  "longitude",
+  "altitude",
+  "timezone",
+  "dst",
+  "tz"
+];
 
-var readStream = fs.createReadStream('airports.dat');
-var writeStream = fs.createWriteStream('airports.json');
+var readStream = fs.createReadStream("airports.dat");
+var writeStream = fs.createWriteStream("airports.json");
 
-var transformer = csv.transform(function(data) {
-  return _.object(columns, data);
+var parse = csv({
+  headers: columns,
+  raw: false
 });
 
 readStream
-  .pipe(csv.parse())
-  .pipe(transformer)
+  .pipe(parse)
+  .on("data", function(data) {
+    data.latitude = parseFloat(data.latitude);
+    data.longitude = parseFloat(data.longitude);
+    data.altitude = parseInt(data.altitude);
+  })
   .pipe(JSONStream.stringify())
   .pipe(writeStream);
